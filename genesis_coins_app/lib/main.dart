c70cb691b77b8650efd9ab171f98e8c2720727cc
Fernando Britto <me@fernandobritto.com.br>
2019-06-02 10:09:49 -0300

    testando a API

--- genesis_coins_app/lib/main.dart
+++ genesis_coins_app/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
 
-void main(){
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=SUA-CHAVE ";
+
+void main() async {
+
+  http.Response response = await http.get(request);
+  print(json.decode(response.body));
+  //print(json.decode(response.body)["results"]["currencies"]);
+
   runApp(MaterialApp(
     home: Container(),
   ));
