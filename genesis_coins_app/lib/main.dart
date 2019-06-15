1a06d04b8143696dc9e0d70557c122bf69cd56f4
Fernando Britto <me@fernandobritto.com.br>
2019-06-15 20:15:20 -0300

    Bibliotecas

--- genesis_coins_app/lib/main.dart
+++ genesis_coins_app/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
 
-
+const request = "https://api.hgbrasil.com/finance?format=json&key=SUA-CHAVE";
 
 void main(){
+
+  http.Response response = http.get(request);
+
   runApp(MaterialApp(
     home: Container(),
-  ));
+  )); // MaterialApp
 }
